3d4b03f833c42d4153ed04d7241c364990c7e3fb
James Lin <b04901069@ntu.edu.tw>
2019-02-13 22:15:09 +0800

    add experiment

--- experiment.sh
+++ experiment.sh
@@ -1,1 +1,2 @@
-python3 main.py train ae_01 -epoch_num 100 -save ae_01.pkl -log ae_01.txt
+# python3 main.py train ae_01 -epoch_num 100 -save ae_01.pkl -log ae_01.txt
+python3 main.py train ae_02 -epoch_num 100 -save ae_02.pkl -log ae_02.txt
