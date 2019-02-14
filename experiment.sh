1654c6acb1ac9f0e9b024ccb2b7391bfc1661e0a
James Lin <b04901069@ntu.edu.tw>
2019-02-15 01:00:45 +0800

    edit

--- experiment.sh
+++ experiment.sh
@@ -9,7 +9,7 @@
 # python3 main.py predict vae_01 -load ../weights/vae_01_03.pkl -predict_dir prediction/vae_01_03/
 # python3 main.py train vae_01 -epoch_num 150 -save vae_01_04.pkl -log vae_01_04.txt -sigma 0.0001
 # python3 main.py predict vae_01 -load ../weights/vae_01_04.pkl -predict_dir prediction/vae_01_04/
-python3 main.py train vae_02 -epoch_num 150 -save vae_02_01.pkl -log vae_02_01.txt -sigma 0.001
-python3 main.py predict vae_02 -load ../weights/vae_02_01.pkl -predict_dir prediction/vae_02_01/
+# python3 main.py train vae_02 -epoch_num 150 -save vae_02_01.pkl -log vae_02_01.txt -sigma 0.001
+# python3 main.py predict vae_02 -load ../weights/vae_02_01.pkl -predict_dir prediction/vae_02_01/
 python3 main.py train vae_02 -epoch_num 150 -save vae_02_02.pkl -log vae_02_02.txt -sigma 0.0001
 python3 main.py predict vae_02 -load ../weights/vae_02_02.pkl -predict_dir prediction/vae_02_02/
