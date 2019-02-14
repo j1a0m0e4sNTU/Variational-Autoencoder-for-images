e1188cf758e58c82b885d893e9e3adf4500973b3
James Lin <b04901069@ntu.edu.tw>
2019-02-15 02:03:37 +0800

    new exp

--- experiment.sh
+++ experiment.sh
@@ -2,14 +2,14 @@
 # python3 main.py predict ae_01 -load ../weights/ae_01.pkl -predict_dir prediction/ae_01/
 # python3 main.py train ae_02 -epoch_num 100 -save ae_02.pkl -log ae_02.txt
 # python3 main.py train vae_01 -epoch_num 500 -save vae_01_01.pkl -log vae_01_01.txt -sigma 0.5
-# python3 main.py predict vae_01 -load ../weights/vae_01_01.pkl -predict_dir prediction/vae_01_01/
+python3 main.py predict vae_01 -load ../weights/vae_01_01.pkl -predict_dir prediction/vae_01_01/
 # python3 main.py train vae_01 -epoch_num 500 -save vae_01_02.pkl -log vae_01_02.txt -sigma 0.05
-# python3 main.py predict vae_01 -load ../weights/vae_01_02.pkl -predict_dir prediction/vae_01_02/
+python3 main.py predict vae_01 -load ../weights/vae_01_02.pkl -predict_dir prediction/vae_01_02/
 # python3 main.py train vae_01 -epoch_num 150 -save vae_01_03.pkl -log vae_01_03.txt -sigma 0.001
-# python3 main.py predict vae_01 -load ../weights/vae_01_03.pkl -predict_dir prediction/vae_01_03/
+python3 main.py predict vae_01 -load ../weights/vae_01_03.pkl -predict_dir prediction/vae_01_03/
 # python3 main.py train vae_01 -epoch_num 150 -save vae_01_04.pkl -log vae_01_04.txt -sigma 0.0001
-# python3 main.py predict vae_01 -load ../weights/vae_01_04.pkl -predict_dir prediction/vae_01_04/
+python3 main.py predict vae_01 -load ../weights/vae_01_04.pkl -predict_dir prediction/vae_01_04/
 # python3 main.py train vae_02 -epoch_num 150 -save vae_02_01.pkl -log vae_02_01.txt -sigma 0.001
-# python3 main.py predict vae_02 -load ../weights/vae_02_01.pkl -predict_dir prediction/vae_02_01/
-python3 main.py train vae_02 -epoch_num 150 -save vae_02_02.pkl -log vae_02_02.txt -sigma 0.0001
+python3 main.py predict vae_02 -load ../weights/vae_02_01.pkl -predict_dir prediction/vae_02_01/
+# python3 main.py train vae_02 -epoch_num 150 -save vae_02_02.pkl -log vae_02_02.txt -sigma 0.0001
 python3 main.py predict vae_02 -load ../weights/vae_02_02.pkl -predict_dir prediction/vae_02_02/
