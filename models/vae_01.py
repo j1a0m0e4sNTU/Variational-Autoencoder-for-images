7c7ec0883048556aa664a8ef6973e063671337a5
James Lin <b04901069@ntu.edu.tw>
2019-02-15 19:37:38 +0800

    edit

--- models/vae_01.py
+++ models/vae_01.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 def Conv2d(in_planes, out_planes, kernel_size = 3):
     layer = nn.Sequential(
@@ -71,7 +72,8 @@ class Model(nn.Module):
         return out, mu, logvar
     
     def sample(self, batch_size):
-        latent = torch.rand(batch_size, self.latent).type(torch.float).cuda()
+        rands = np.random.normal(0.5,1 ,(batch_size, self.latent))
+        latent = torch.tensor(rands).type(torch.float).cuda()
         # latent = (latent) * 2 
         out = self.fc_mu(latent)
         out = out.view(batch_size, self.latent, 1, 1)
