1755bdf5e3e4a035adeda62248bac284aa375aa5
James Lin <b04901069@ntu.edu.tw>
2019-02-16 02:21:57 +0800

    t

--- models/vae_02.py
+++ models/vae_02.py
@@ -73,8 +73,8 @@ class Model(nn.Module):
     
     def sample(self, batch_size):
         latent = torch.rand(batch_size, self.latent).type(torch.float).cuda()
-        out = self.fc_mu(latent)
-        out = out.view(batch_size, self.latent, 1, 1)
+        # out = self.fc_mu(latent)
+        out = latent.view(batch_size, self.latent, 1, 1)
         out = self.decoder(out)
         return out
         
